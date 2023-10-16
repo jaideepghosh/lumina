1d210b5255780cba479d94beb400f75828d3e482
roopamsrivastava <er.roopamsrivastava@gmail.com>
2023-10-16 15:09:46 +0530

    feat:Added gallery in the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Gallery />
     </div>
   );
 }
