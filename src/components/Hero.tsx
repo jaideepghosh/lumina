9ebe37a02fc565dd777abed0613d072954deb31d
jaideepghosh <er.jaideepghosh@gmail.com>
2023-10-16 16:28:03 +0530

    fix:Hero component padding.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => (
   <section className="bg-white dark:bg-gray-900">
-    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+    <div className="pt-16 pb-8 px-4 mx-auto max-w-screen-xl text-center">
       <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Illuminating Innovation
       </h1>
@@ -8,7 +8,7 @@ const Hero = () => (
         Unlock the Power of SaaS: Your Gateway to Inspiration, Development
         Insights, and Entrepreneurial Vision.
       </p>
-      <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+      <div className="flex flex-col mb-2 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
         <a
           href="#"
           className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
